9c646c9fcb6bf715f169dcc69d9abe8555ee4f80
Lukatar <lukatar@Ubuntu.homet.virtualbox.org>
2023-04-25 01:08:13 +0300

    replace array of prime numbers with isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,14 +2,24 @@
 import gameLogic from '../index.js';
 import greeting from '../greeting.js';
 
+const isPrime = (n) => {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i < n / 2; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 export default () => {
   const userName = greeting();
-  const arrOfPrimeNum = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-    73, 79, 83, 89, 97];
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   for (let i = 1; i <= 3; i += 1) {
     const randomNum = Math.floor(Math.random() * 100 + 1);
-    const rightAnswer = arrOfPrimeNum.includes(randomNum) ? 'yes' : 'no';
+    const rightAnswer = isPrime(randomNum) ? 'yes' : 'no';
     const result = gameLogic(rightAnswer, userName, randomNum, i);
     if (!result) {
       break;
