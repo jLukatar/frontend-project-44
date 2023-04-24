fcf350544e1be48484b5bf02a13e96312b323271
Lukatar <lukatar@Ubuntu.homet.virtualbox.org>
2023-04-25 01:33:13 +0300

    move switch to separate module fix switch

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,6 +1,19 @@
 import gameLogic from '../index.js';
 import greeting from '../greeting.js';
 
+const math = (operation, firstNum, secondNum) => {
+  switch (operation) {
+    case '+':
+      return firstNum + secondNum;
+    case '-':
+      return firstNum - secondNum;
+    case '*':
+      return firstNum * secondNum;
+    default:
+      throw new Error(`Unknown operation: '${operation}'!`);
+  }
+};
+
 export default () => {
   const userName = greeting();
   console.log('What is the result of the expression?');
@@ -8,23 +21,9 @@ export default () => {
   for (let i = 1; i <= 3; i += 1) {
     const firstNum = Math.floor(Math.random() * 11);
     const secondNum = Math.floor(Math.random() * 11);
-    const Operation = mathOperations[Math.floor(Math.random() * 3)];
-    const mathProblem = `${firstNum} ${Operation} ${secondNum}`;
-    let correctAnswer;
-    switch (Operation) {
-      case '+':
-        correctAnswer = firstNum + secondNum;
-        break;
-      case '-':
-        correctAnswer = firstNum - secondNum;
-        break;
-      case '*':
-        correctAnswer = firstNum * secondNum;
-        break;
-      default:
-        break;
-    }
-    correctAnswer = correctAnswer.toString();
+    const operation = mathOperations[Math.floor(Math.random() * 3)];
+    const mathProblem = `${firstNum} ${operation} ${secondNum}`;
+    const correctAnswer = math(operation, firstNum, secondNum).toString();
     const result = gameLogic(correctAnswer, userName, mathProblem, i);
     if (!result) {
       break;
