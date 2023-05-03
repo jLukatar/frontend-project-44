49461587edae30da6100ce6e41a8611cd02e353c
Lukatar <lukatar@Ubuntu.homet.virtualbox.org>
2023-05-04 01:05:48 +0300

    brain-games.js_fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import startGame from '../src/games/brain-calc.js';
+import askName from '../src/cli.js';
 
-startGame();
+console.log('Welcome to the Brain Games!');
+askName();
